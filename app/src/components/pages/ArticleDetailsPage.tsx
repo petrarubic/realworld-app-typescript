cc311e5b852eb3423dc35c9d79b8ddd6abfa0a4b
petra.rubic <prubic.dev@gmail.com>
2023-07-13 07:17:32 +0200

    Add user following functionality

--- app/src/components/pages/ArticleDetailsPage.tsx
+++ app/src/components/pages/ArticleDetailsPage.tsx
@@ -4,14 +4,43 @@ import { Article } from '../../types/Article'
 import { fetchArticle } from '../../service/articleService'
 import Spinner from '../Spinner'
 import { formatDateString } from '../../utils/utils'
+import { followUser, unfollowUser } from '../../service/profileService'
+import { UserMinusIcon, UserPlusIcon } from '../../icons'
+import { useEffect, useState } from 'react'
+import { Tooltip } from 'react-tooltip'
+import { fetchCurrentUser } from '../../service/authService'
 
 function ArticleDetailsPage() {
   const { slug } = useParams()
+  const [showFollowButton, setShowFollowButton] = useState(false)
 
   const { isLoading, isError, data, error } = useQuery<Article, Error>({
     queryKey: ['article', slug],
     queryFn: () => fetchArticle(slug),
   })
+
+  const [followedUser, setFollowedUser] = useState(data?.author)
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userData = await fetchCurrentUser()
+        if (
+          userData &&
+          data?.author.username &&
+          userData.username === data?.author.username
+        ) {
+          setShowFollowButton(false)
+        } else {
+          setShowFollowButton(true)
+        }
+      } catch (error) {
+        console.error('Failed to fetch current user:', error)
+      }
+    }
+
+    fetchUserData()
+  }, [data?.author.username])
 
   if (isLoading) {
     return (
@@ -31,12 +60,71 @@ function ArticleDetailsPage() {
 
   if (data) {
     const formattedBody = data.body.replace(/\\n/g, ' ')
+
+    const handleFollowUser = async () => {
+      try {
+        const updatedAuthor = await followUser(data.author)
+        setFollowedUser(updatedAuthor)
+      } catch (error) {
+        console.error('Error with following the selected article author', error)
+      }
+    }
+
+    const handleUnfollowUser = async () => {
+      try {
+        const updatedAuthor = await unfollowUser(data.author)
+        setFollowedUser(updatedAuthor)
+      } catch (error) {
+        console.error(
+          'Error with unfollowing the selected article author',
+          error
+        )
+      }
+    }
+
     return (
       <div className='flex justify-center items-center'>
         <div className='w-full px-8 md:px-32 xl:px-96 py-10'>
-          <div className='pb-5'>
-            <p className='font-bold text-xl'>{data.author.username}</p>
-            <p className='text-gray-500'>{formatDateString(data.createdAt)}</p>
+          <Tooltip
+            id='follow-tooltip'
+            style={{
+              padding: '4px',
+              fontSize: '10px',
+            }}
+          />
+          <div className='flex flex-row space-x-4'>
+            <img
+              src={data.author.image ? data.author.image : ''}
+              alt='Profile image'
+              className='rounded-full w-14 h-14'
+            />
+            <div className='pb-5'>
+              <p className='font-bold text-xl flex items-center space-x-1'>
+                <span>{data.author.username}</span>
+                {showFollowButton && (
+                  <button
+                    data-tooltip-id='follow-tooltip'
+                    data-tooltip-content={
+                      !followedUser?.following ? 'Follow User' : 'Unfollow User'
+                    }
+                    onClick={() => {
+                      !followedUser?.following
+                        ? handleFollowUser()
+                        : handleUnfollowUser()
+                    }}
+                  >
+                    {!followedUser?.following ? (
+                      <UserPlusIcon />
+                    ) : (
+                      <UserMinusIcon />
+                    )}
+                  </button>
+                )}
+              </p>
+              <p className='text-gray-500'>
+                {formatDateString(data.createdAt)}
+              </p>
+            </div>
           </div>
           <p className='font-extrabold text-4xl mb-5'>{data.title}</p>
           <p className='text-xl text-gray-700 mb-5'>{data.description}</p>
--- app/src/service/profileService.tsx
+++ app/src/service/profileService.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { User } from '../types/User'
 import { ProfileFormData } from '../types/ProfileFormData'
+import { Author } from '../types/Author'
 
 const baseUrl = 'https://api.realworld.io/api'
 
@@ -25,6 +26,55 @@ export const editProfile = async (data: ProfileFormData): Promise<User> => {
       return Promise.reject('Failed to edit current user')
     }
     console.error('Edit current user error:', error)
+    return Promise.reject(error)
+  }
+}
+
+// Follow selected user
+export const followUser = async (author: Author): Promise<Author> => {
+  try {
+    const token = localStorage.getItem('userToken')
+    const authHeader = token?.replace(/^"(.*)"$/, '$1')
+    const headers = {
+      Authorization: `Token ${authHeader}`,
+    }
+
+    const res = await axios.post(
+      `${baseUrl}/profiles/${author.username}/follow`,
+      {},
+      { headers }
+    )
+    return res.data.profile
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Axios follow user request error:', error)
+      return Promise.reject('Failed to follow the user')
+    }
+    console.error('Follow the user error:', error)
+    return Promise.reject(error)
+  }
+}
+
+// Unfollow selected user
+export const unfollowUser = async (author: Author): Promise<Author> => {
+  try {
+    const token = localStorage.getItem('userToken')
+    const authHeader = token?.replace(/^"(.*)"$/, '$1')
+    const headers = {
+      Authorization: `Token ${authHeader}`,
+    }
+
+    const res = await axios.delete(
+      `${baseUrl}/profiles/${author.username}/follow`,
+      { headers }
+    )
+    return res.data.profile
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Axios unfollow user request error:', error)
+      return Promise.reject('Failed to unfollow the user')
+    }
+    console.error('Unfollow the user error:', error)
     return Promise.reject(error)
   }
 }
